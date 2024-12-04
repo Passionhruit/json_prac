54de94b16894a8e397bc54acae39d923ffec4b06
Passionhruit <korea_stan@naver.com>
2024-12-04 10:53:55 +0900

    feat: json-server 셋팅 및 update, get 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    // 유저 불러오기
+    const getUsers = async () => {
+      const response = await axios.get(`http://localhost:4000/users`);
+      setUsers(response.data);
+    };
+
+    getUsers();
+  }, []);
+
+  // 유저 수정하기
+
+  const updateUser = async (id, body) => {
+    console.log(id, body);
+    await axios.patch(`http://localhost:4000/users/${id}`, body);
+    window.location.reload();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map((user) => (
+        <>
+          <div>고유번호 : {user.id}</div>
+          <div>이름 : {user.name}</div>
+          <div>이메일 : {user.email}</div>
+        </>
+      ))}
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        onClick={() =>
+          updateUser(1, {
+            id: 1,
+            name: input,
+            email: "John@gmail.com",
+          })
+        }
+      >
+        유저 이름 변경하기
+      </button>
     </div>
   );
 }
